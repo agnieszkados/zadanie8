25f0e62f8d394660895eeb898859dde8f8488daa
agnieszkados <agnieszkados@o2.pl>
2017-05-02 17:57:39 +0200

    nowe

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+var a = prompt('Wpisz dl boku');
+alert('bok ma dl,' + a);
+var h = prompt('wpisz wysokosc');
+alert('wysokosc to,' + h);
+var triangleArea = a*h/2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+//dodaję kolejny komentarz
